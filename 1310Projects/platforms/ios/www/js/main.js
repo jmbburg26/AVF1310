38cecfb4e6c36ee4c388d7a864d195b830d8e1f0
John Brandenburg <jmbburg26@fullsail.edu>
2013-10-11 21:13:42 -0400

    Able to pull data from Instagram. Images not displaying in search results.

--- 1310Projects/platforms/ios/www/js/main.js
+++ 1310Projects/platforms/ios/www/js/main.js
@@ -1,13 +1,16 @@
-//Function to get the user search term from search field
-var storeData = function(){
-
-		var	searchTag = $('#searchtag').val();
-
-		var url = "https://api.instagram.com/v1/media/" + searchtag + "?lat=48.858844&lng=2.294351&?access_token=47395760.f59def8.0c2a11ced6a74f0bb5088750077bf5a7";
+$('#submit').on('click', function(){
+ 	var	searchTag = $('#searchtag').val();
+		//var tag = "kittens";
+		console.log(searchTag);
+		
+		var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550&amp;min_id=5";
 		
 		console.log(url);
 
-	var screenOutput = function(info){
+		$.getJSON(url, screenOutput);
+});
+
+var screenOutput = function(info){
 
 		alert("Search Complete");
 		console.log(info);
@@ -15,15 +18,9 @@ var storeData = function(){
 		$("#data-msg").html("<h2>Search results:</h2>");
 
 		$.each(info.data, function(index, photo){
-			var searchPic = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
+			var pic = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
 
-			$("#data-output").append(searchPic);
+			$("#data-output").append(pic);
 		});
-	};
-    $.getJSON(url, screenOutput());
 };
 
-
-$('#submit').on('click', function(){
-    storeData();
-});
