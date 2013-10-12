31dafc7dfe9224f2bab1449d8b8277e80d72d7d0
John Brandenburg <jmbburg26@fullsail.edu>
2013-10-11 22:39:45 -0400

    Added USA today api call.

--- 1310Projects/platforms/android/assets/www/js/main.js
+++ 1310Projects/platforms/android/assets/www/js/main.js
@@ -1,42 +1,24 @@
-$('#submit').on('click', function (){
-    storeData();
-    console.log(searchTag);
-});
+$('#submit').on('click', function(){
+ 	var	searchTag = $('#searchtag').val();
+		console.log(searchTag);
+		
+		var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550";
+		console.log(url);
 
-//Function to get the user search term from search field
-var storeData = function (key){
-    if(!key){
-            var userValues      = Math.floor(Math.random()*100000001);
-        }else{
-            userValues = key;
-        }
-      
-    var key    = Math.floor(Math.random()*100000001);
-
-    var	searchTag = $('#searchtag').val();
-
-    localStorage.setItem(key, JSON.stringify(searchTag));
-    //console.log(searchTag);
-    return searchTag
-};
-
-var searchResults = $(function(searchTag){
-
-	var url = "https://api.instrgram.com/vl/tags/" + searchTag + "media/recent?callback=?&amp;client_id=xxxxxx";
-	
-	$.getJSON(url, screenOutput);
+		$.getJSON(url, screenOutput);
 });
 
 var screenOutput = function(info){
 
-	alert("Search Complete");
-	console.log(info);
+		alert("Search Complete");
+		console.log(info);
 
-	$("#data-mesg").html("<h2>Search results:</h2>");
+		$("#data-msg").html("<h2>Search results:</h2>");
 
-	$.each(info.data, function(index, photo){
-		var searchPic = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
+		$.each(info.data, function(index, photo){
+			var pic = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
 
-		$("data-output").append(searchPic);
-	});
+			$("#data-output").append(pic);
+		});
 };
+
--- 1310Projects/platforms/ios/www/js/main.js
+++ 1310Projects/platforms/ios/www/js/main.js
@@ -1,12 +1,11 @@
+//Code for Instagram API
 $('#submit').on('click', function(){
  	var	searchTag = $('#searchtag').val();
-		//var tag = "kittens";
 		console.log(searchTag);
 		
-		var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550&amp;min_id=5";
-		
+		var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550&amp;min_id=10";
 		console.log(url);
-
+		
 		$.getJSON(url, screenOutput);
 });
 
@@ -24,3 +23,28 @@ var screenOutput = function(info){
 		});
 };
 
+
+
+$('#news').on('pageinit', function(){
+        //Code for USA Today
+		$(function(){
+				var url = "http://api.usatoday.com/open/articles/mobile/topnews?api_key=rafzauu4bcfd33yg379mjn9e";
+				console.log(url);
+
+				$.getJSON(url, screenOutput);
+		});
+
+		var screenOutput = function(info){
+
+				alert("Done Loading");
+				console.log(info);
+
+				$("#data-msg").html("<h2>Top Stories:</h2>");
+
+				$.each(info.data, function(index, photo){
+					var news = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
+
+					$("#news-output").append(news);
+				});
+		};
+});
