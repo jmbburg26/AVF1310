f608c4062768d539e5ef2a70aa6ca141865cc1ce
John Brandenburg <jmbburg26@fullsail.edu>
2013-10-23 00:08:29 -0400

    Got all Native Features working on iOS and Instagram API

--- 1310Projects/platforms/ios/www/js/main.js
+++ 1310Projects/platforms/ios/www/js/main.js
@@ -8,7 +8,6 @@
     	$("#getpicture").on('click', getPic);
     	$("#getgeolocation").on('click', getLocation);
     	$("#compasstest").on('click', getCompass);
-    	$('#submit').on('click', getInstagram;
     }
 
     function alertDismissed() {
@@ -101,58 +100,55 @@
 	};
 
 
-
-
 //Code for Instagram API
-var getInstagram = function(){
- 	var	searchTag = $('#searchtag').val();
-		
-		console.log(searchTag);
-		event.preventDefault();
-		
-		var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550&amp;min_id=10";
-		console.log(url);
-		
-		$.getJSON(url, screenOutput);
+$('#submit').on('click', function(){
+    var searchTag = $('#searchtag').val();
+        
+        console.log(searchTag);
+        event.preventDefault();
+        
+        var url = "https://api.instagram.com/v1/tags/" + searchTag + "/media/recent?callback=?&amp;client_id=fc637f7a1bd04468be5f4fd4bbbea550&amp;min_id=10";
+        console.log(url);
+                
+        $.getJSON(url, screenOutput);
 });
 
 var screenOutput = function(info){
 
-		alert("Search Complete");
-		console.log(info);
+    alert("Search Complete");
+    console.log(info);
 
-		$("#data-msg").html("<h2>Search results:</h2>");
+    $("#data-msg").html("<h2>Search results:</h2>");
 
-		$.each(info.data, function(index, photo){
-			var pic = "<li><img src='" + photo.images.low_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>'";
+    $.each(info.data, function(index, photo){
+        var pic = "<li><img src='" + photo.images.thumbnail.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>'";
 
-			$("#data-output").append(pic);
-		});
+        $("#data-output").append(pic);
+    });
 };
 
 
 
 $('#news').on('pageinit', function(){
-        
         //Code for USA Today
-		$(function(){
-				var url = "http://api.usatoday.com/open/articles/mobile/topnews?api_key=rafzauu4bcfd33yg379mjn9e";
-				console.log(url);
+                $(function(){
+                                var url = "http://api.usatoday.com/open/articles/mobile/topnews?api_key=rafzauu4bcfd33yg379mjn9e";
+                                console.log(url);
 
-				$.getJSON(url, screenOutput);
-		});
+                                $.getJSON(url, screenOutput);
+                });
 
-		var screenOutput = function(info){
+                var screenOutput = function(info){
 
-				alert("Done Loading");
-				console.log(info);
+                                alert("Done Loading");
+                                console.log(info);
 
-				$("#data-msg").html("<h2>Top Stories:</h2>");
+                                $("#data-msg").html("<h2>Top Stories:</h2>");
 
-				$.each(info.data, function(index, news){
-					var news = "<li><a src='" + http://api.usatoday.com/open/articles/mobile/topnews?api_key=rafzauu4bcfd33yg379mjn9e"' /></li>";
+                                $.each(info.data, function(index, photo){
+                                        var news = "<li><img src=" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
 
-					$("#news-output").append(news);
-				});
-		};
+                                        $("#news-output").append(news);
+                                });
+                };
 });
